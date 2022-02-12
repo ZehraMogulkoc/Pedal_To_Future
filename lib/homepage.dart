e621308da9f65edcffebd1c3d360f522ca86f436
BeyzanurYuce <94442443+BeyzanurYuce@users.noreply.github.com>
2022-02-12 22:15:37 +0300

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'prizes.dart';
+import 'bottom_appbar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,20 +12,37 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      decoration: BoxDecoration(image: DecorationImage(image:AssetImage('assets/images/bg5.jpg'),fit: BoxFit.fill),),
-      child: SafeArea(
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => BottomNavBar()));
+      },
+      child: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(image: DecorationImage(image:AssetImage('assets/images/bg5.jpg'),fit: BoxFit.cover),),
         child: Container(
           height: double.infinity,
           width: double.infinity,
           color: Colors.black.withOpacity(0.6),
-          child: Center(child: Text('Hos geldin User! Hadi baslayalim',style: TextStyle(
-            color: Colors.white,
-            fontFamily: 'Architects Daughter',
-            fontSize: 24,
-          ),)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Hos geldin User! Hadi baslayalim',style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Architects Daughter',
+                fontSize: 20,
+              ),),
+              SizedBox(height: 50,),
+              Text('(Devam etmek icin ekrana dokun)',style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Architects Daughter',
+                fontSize: 15,
+              ),),
+
+            ],
+          ),
         ),
       ),
     );
