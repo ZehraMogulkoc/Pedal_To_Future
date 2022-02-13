db5833a30d74f11dcff3941cd72d98a703c47b64
ZehraMogulkoc <87859856+ZehraMogulkoc@users.noreply.github.com>
2022-02-13 13:21:43 +0300

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'prizes.dart';
-import 'bottom_appbar.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,39 +13,28 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: (){
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => BottomNavBar()));
-      },
-      child: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: BoxDecoration(image: DecorationImage(image:AssetImage('assets/images/bg5.jpg'),fit: BoxFit.cover),),
+    final user=FirebaseAuth.instance.currentUser;
+
+
+    var currentUser=FirebaseFirestore.instance.collection('Data').doc(user?.email);
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      decoration: BoxDecoration(image: DecorationImage(image:AssetImage('assets/images/bg5.jpg'),fit: BoxFit.fill),),
+      child: SafeArea(
         child: Container(
           height: double.infinity,
           width: double.infinity,
           color: Colors.black.withOpacity(0.6),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Hos geldin User! Hadi baslayalim',style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'Architects Daughter',
-                fontSize: 20,
-              ),),
-              SizedBox(height: 50,),
-              Text('(Devam etmek icin ekrana dokun)',style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'Architects Daughter',
-                fontSize: 15,
-              ),),
-
-            ],
-          ),
+          child: Center(child: StreamBuilder(stream: currentUser.snapshots(),builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+    if(snapshot.hasData){
+    return Text("Hoşgeldin ${snapshot.data.data()['Name']}!  Haydi başlayalım ",style: TextStyle(color: Colors.white,fontSize: 40),); }
+    else{
+    return CircularProgressIndicator();
+    }
+    }),
         ),
       ),
-    );
+    ));
   }
 }
